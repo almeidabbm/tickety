3c5a60dbb79e757912edf81dd1f0b2a317c1869b
almeidabbm <almeidabatistam@gmail.com>
2023-06-18 18:45:07 +0100

    feat: add counter on enter keyDown

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 const { app, BrowserWindow } = require("electron");
+const MAX_TICKET_NUM = 99;
+
+let ticket = 0;
+
+function nextTicket() {
+  if (ticket >= MAX_TICKET_NUM) {
+    ticket = 0;
+  } else {
+    ticket++;
+  }
+
+  return ticket;
+}
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+  });
+
+  win.webContents.on("before-input-event", (e, i) => {
+    if (i.key === "Enter" && i.type === "keyDown") {
+      const currentTicket = nextTicket();
+      console.log(`Current ticket is ${currentTicket}`);
+    }
   });
 
   win.loadFile("index.html");
